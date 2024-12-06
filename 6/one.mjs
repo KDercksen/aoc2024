ed0059308eee2e99835604d3b36e6aff4e7d1800
KDercksen <mail@koendercksen.com>
2024-12-06 08:44:56 +0100

    Add initial implementation for maze navigation using guard position

---
+++ 6/one.mjs
@@ -0,0 +1,49 @@
+import fs from "fs";
+
+const data = fs
+  .readFileSync("data.txt", "utf8")
+  .split("\n")
+  .map((line) => line.split(""));
+
+// find the guard position (character "^")
+const guardRow = data.findIndex((line) => line.includes("^"));
+const guardCol = data[guardRow].findIndex((char) => char === "^");
+const startPos = [guardRow, guardCol];
+const direction = [-1, 0];
+
+const turnRight = (direction) => {
+  const [row, col] = direction;
+  return [col, -row];
+};
+
+const peek = (pos, direction) => {
+  return data[pos[0] + direction[0]][pos[1] + direction[1]];
+};
+
+const outOfBounds = (pos, direction) => {
+  return (
+    pos[0] + direction[0] < 0 ||
+    pos[0] + direction[0] >= data.length ||
+    pos[1] + direction[1] < 0 ||
+    pos[1] + direction[1] >= data[0].length
+  );
+};
+
+let currentPos = startPos;
+let currentDirection = direction;
+const visited = new Set();
+visited.add(currentPos.join(","));
+while (!outOfBounds(currentPos, direction)) {
+  const nextPos = peek(currentPos, currentDirection);
+  if (nextPos === "#") {
+    currentDirection = turnRight(currentDirection);
+  } else {
+    currentPos = [
+      currentPos[0] + currentDirection[0],
+      currentPos[1] + currentDirection[1],
+    ];
+    visited.add(currentPos.join(","));
+  }
+}
+
+console.log(visited.size - 1);
